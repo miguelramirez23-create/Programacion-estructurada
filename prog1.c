7fe633ca2b9e681ff2f93a7737399105efe89c5d
miguelramirez23-create <miguel.ramirez23@uabc.edu.mx>
2026-08-21 13:30:29 -0700

    Add files via upload

---
+++ prog1.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int main(){
+
+ char letra='L';
+ int numero = 20;
+ float num =  4.0/3;
+ char cadena[20]={"Hola Mundo"}; 
+ /* bool bandera = false; */
+
+ printf("Num: %d  Tipo char --> %c  %c\n",letra, letra,letra+1);
+ printf("numero = %d \n", numero);
+ printf("tipo flotante --> num = %3.4f \n", num);
+
+ printf("Desplegado de una cadena (string) --> %s \n", cadena);
+
+
+
+ return 0;
+}
+
+
+
