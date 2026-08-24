108e11d68fee95a4ef883849ad417288b3c5228f
miguelramirez23-create <miguel.ramirez23@uabc.edu.mx>
2026-08-24 03:02:36 -0700

    prog2.c
    Fix variable names and correct syntax errors in calculations.

--- prog2.c
+++ prog2.c
@@ -1,1 +1,20 @@
-in
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+  float Largo, Ancho, Area, Precio;
+  int caja;
+printf("Largo de la habitacion en metros:");
+scanf("%f",&Largo);
+printf("Ancho de la habitacion en metros:");}
+scanf("%f;Ancho);
+area=Largo*Ancho;
+caja=(int)(area/4)+1;
+precio=caja*452.50;
+printf("\nArea de la habitacion: %.3f m2\n",Area);
+printf("Cajas necesarias: %d\n",cajas);
+printf(Precio Total: %.3f\n",precio);
+  return 0;
+  }
+
