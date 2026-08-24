690a2bcc40deac21e75e2cbd7ada63bf8be9abdd
miguelramirez23-create <miguel.ramirez23@uabc.edu.mx>
2026-08-24 03:27:56 -0700

    Update prog2.c

--- prog2.c
+++ prog2.c
@@ -3,18 +3,19 @@
 
 int main()
 {
+  /*Entradas tipo flotante largo, ancho, Salidas area, precio salida de tipo int cajas*/
   float Largo, Ancho, Area, Precio;
   int caja;
 printf("Largo de la habitacion en metros:");
 scanf("%f",&Largo);
 printf("Ancho de la habitacion en metros:");}
 scanf("%f;Ancho);
-area=Largo*Ancho;
-caja=(int)(area/4)+1;
-precio=caja*452.50;
+area=Largo*Ancho; /* Se calcula el area  de la habitacion*/
+caja=(int)(area/4)+1;/* se calculan las cajas necesarias mas 1 */
+precio=caja*452.50; /* se calcula el precio total a pagar*/
 printf("\nArea de la habitacion: %.3f m2\n",Area);
 printf("Cajas necesarias: %d\n",cajas);
-printf(Precio Total: %.3f\n",precio);
+printf(Precio Total: %.3f\n",precio); 
   return 0;
   }
 
