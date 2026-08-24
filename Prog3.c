3c6129a98c8ebfca1877184f6c5a565941c05d26
miguelramirez23-create <miguel.ramirez23@uabc.edu.mx>
2026-08-24 11:37:49 -0700

    Prog3.c
    Fix syntax errors and improve variable handling in ramp calculation.

---
+++ Prog3.c
@@ -0,0 +1,13 @@
+#include<studio.h>
+#include<stdlib.h>
+int main()
+{
+  float altura, base, largo; /*entradas de tipo flotante altura y base */
+printf("altura de la rampa: ");/*altura de la rampa*/
+scanf("%f", &alrura);
+printf("base de la rampa: ");/*base de la rampa*/
+scanf("%F, &base);
+  largo=sqrt((altura*altura)+(base*base));/*Se resuelve con el metodo pitagoras*/
+  printf("El largo de la rampa es: %3f\n", largo);/*resultado de la operacion */
+return 0
+  }
